6aeb6f9c1c426a16588f1f081525dcbd22d93799
Leo <leonardo.vanzin@hotmail.com>
2024-12-17 18:03:51 -0300

    Ajuste do frame na tela

--- src/app/powerbi-report/powerbi-report.component.ts
+++ src/app/powerbi-report/powerbi-report.component.ts
@@ -18,11 +18,11 @@ export class PowerbiReportComponent implements AfterViewInit {
   // Configuração do relatório Power BI
   embedConfig = {
     type: 'report', // Tipo do embed (relatório)
-    id: 'fa000586-a94a-4c01-a916-3577dcffe835', // ID do relatório
+    id: 'fa000586-a94a-4c01-a916-3577dcffe835', // Report ID
     embedUrl:
       'https://app.powerbi.com/reportEmbed?reportId=fa000586-a94a-4c01-a916-3577dcffe835', // URL de embed
     accessToken:
-      'H4sIAAAAAAAEAB2Uta7EVgBE_-W1jmSmSCkM17zrNUNnZl5jlH_PS_pp5hzN_P3zSe5-SvKfP39od_y23upa17XN-5CqbjZaZNk56kpR_T4zHH5MN_KiTW0bMl_QomgJZL4J6IY6Dsje0jc70PSb1An1nuSsSaGXT9GGqakDyFahCXJ-l8pOz8iaKPnwU9dpoSv1tJEqKlsMG3E1KNW4XWTnwtek86mgzkxU1kDlZlJSfQqAZi5zlgS4Cf3FhJnX8dPCUx0koNGbzDGDgz1vYBf5eYZ1Oy6RKPiVKCjLsDUxN8kFTnvHG3tou3G8hLO-royhfSgVt4U6BJzo729vOza0M5Bvlm6b575Q-XwBLfaAno8ZaslKseC65RC68eXb51Rf8jbme71-bt9gGUJU2RsyzkwShl7GuoL0dB__UDGTPPIXSFVENJAu6eKxyKK4uUJ3cAY2LfFVJ4P_6GbdRfWDgK2tC25CRbE2eXTCLXyzGrd-U8E6H7cgKZumginPGzZ-F3OVfwP1FFxpT-gWUb8CBuCvJq-BFa1-xhG5D8FZF80TQUeOMi-17tgXIkELw2gSBoia0rrQSyc_xrCyvbH1eHorwD-7-biQ5rOF798JFzowEfYmYyMNEkHwXXZtGPOl9LIsbHVArAFk4iPVnmwZYVpp6KGeia5LWa0IXwrMlnjiTDb7MX2hQD6kuPv0piIbImT9ZiYqwCOBskNJXNrzurcwnH_7o5DS4gfP6e8pbYwye4cneqmPyj7H1fca88J8xjGxdExkokdl5mVXbyv_SGw5dsJcYnTKULR_bYA_RLPQ3ul6IT6yC1KHscTaK-ywFSUBL8BvyTMs94b54lIAvkfQdSylOy8D2XboY8NRFmhdXcKGWD_YSJ4rD9g5pOdyilxSIqYIKn_--BHWe_5OenH_zinMNHE-r8h1eA-eoxk9WbXfu3sXg8hCZGnUq6CkZQ33BUdcHqOOX426gow17OndytqZwoyoFrAuQk8biXvumBnoqyGpwxbDdY9z6Sj6jZJhAIPJm1h2xVbZoQtrJAIkZd02LlLb30NzLxAVSyAzkAGIk7tQEYjm8jiGi_lhgTto2jOn-zLifU9jT3MWYjMyJK5vqJIs9Od6FwXGTYZTWQAvRkKQMLat2SW7FNNL_UGhyVhfOZRPnS6o9UCusYD7qvyjYl53KO73sXe1s-HuLEDEloG23W33hARswBw-ErTQ0H3odCcBTxXLi4nqS6n0hfgcxxOvN5Rn6p1tWxPrdTPWX3_9h_me62JV_V_KKPzGU54FQShW4VpF0rJC3vl_ymmqMfnua_GfjEVL8KeEJMv3nbQ4-rUB-D6vC59gyO75gr_VrOKhK9LBg_eyD0f4iuSbvuQl7N1wdILl0YuGEslJ9TYFL3aO80325SPz5tVQOBx50FQSIjD1RqJNBfXxcgUexxYVmWAvdolM9IFkyf5I2IabFf3azCl_q1KTiP1kT2FlDvPomJfZWmiB2Kwb21RjjDOMdQMF1_wtNJSp-jhndTkHFINUfg_ZIXaccfE0_KC5vZs8Bj6mqGc5aVTRZoApSAGkXl6N7hduDF5j86qCuYmNv2Bx5DUblxiE_Gp-XGZz5niwyr2rupShZIwQ7aMr9l7I4IzJg09bAmJfLdkzlszxyAKPBv6_jH_-BQuXOZpCBgAA.eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLUJSQVpJTC1TT1VUSC1CLVBSSU1BUlktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQiLCJleHAiOjE3MzQ0NjAzNDAsImFsbG93QWNjZXNzT3ZlclB1YmxpY0ludGVybmV0Ijp0cnVlfQ==', // Token de acesso
+    'H4sIAAAAAAAEACWTR4rsWgIF91JTNci7hjdI2ZRP6crP5L1N-c_fe9ej5wEHgjj__HySu5-S_Oe_PxWyN-dwV9QDaEUWVRFF_PcVbznlEn31JbQEdK_DR0v0nBpfdqJFrqB6WKKr7ywMxsoHNl3wYZfekuowk9YXODp-3xtUMHHOLjjMN3gkZHXrhdlxIlzPYLAgAqy0o6qMjCTnP_u8f8RwTd2D0-hGj6Q07MlPrfSZtD8tX6UJUecD2xYHZE7tJgQMU7JeBNwcXSI8uM6L66qjIqWjlQyYE4znS2ci5Xcp00RPVR0wNh4lobAqIcBIl4_WZIccHuB1qCbXbGL9zbco5cPCOr8mmaXZLiat7x5RPUxPB2CywxJzRWQ4PSoH_Yar8avyyICQ5LwGQlRNEA9QJf-mUJH7YjyFicai9rx8Jp51t9tYW6XCJY8kjBuyS-GC3A4TN_3lSdKXDydmfaB0NGZhqvFsK0nWSXDu4kOM09hvPb48495U9wW_n8F-xXqsHL56J_FCndKVe9DgUaE6xFDTOidVNhkgtU6PXLaooZXg0E6TIjp8SDnNAoVZacyJTySHI3tg8ObWxu1OCI1yLyeopp63aYvJgR3cuVlpXNRkOjNR9MFRjzFRXPdxxma2dRYExbnA8oq4Cio7m3rMJYJMOX9ZwgH5UcLI0xLL71dlF_5C4K1fFjsMaeSjI1B5GYmlfAygHGmDNvygfPt6N0u-U-SbGnNaP9SzmwdWUhYBdRUYkZWcgNtYgdhEMcaugaEt00SrlfCzQJKz0Tew63eItXL5fWsHj2ZCrX0NgfI4pEprZLjgAS3zRB0FQdpXc7B-a61qtC8yfK3MKpc3WprVapdSu3vnw5bGhWgO6AIkW0XztkM4NT9pRUnV69HvwksngKinZRnrz39--PWet0kr7t87vRv805kE54wp-oxm1EzBRWjj0W7W2zuXZUZ2ENOP8t4PHG7wfCnUqQYPNgjrdLmUYCS8YOdFR_i0sHjSMzwRBHdkEouBqu4tb4lZCXod_hLf9byIWwllDB7e0Hg5otiE8eE9KXE5JmcCbp98C11G46Z9R2YeJtVinIzT_Chr-GFEPbNlCY7TBeYQINPctdx3UdMQql3XHDjMGNuS-gp-84_f8r6hkjyzS5juO-7Brp8xz3fwd5z-4p72-3QgxwZ40uopN9crpvhDCVTBKmcXLuAEVpKNOaYqUvZZgjsUMi0UTSFTq0OobRE_CmfB8uxTzzYyFFu9yswBQpU64Q9uUOXrz5-_mu-5LlbF_7V8L1AtOFdt8XYkcPJkuJ9X_38KNNWYbPta_GIisXSmVYhx3teNc8oqhXNRJEhoxBqIZUeTePAWAj1xniao71HJZVC0PwTU9lq888Sf4wvAkie0_mA8aIWjrlyj4WFwy6qwlwGR9g0p1CsGaEb2SRdjXVK-c1r9Yn8NtUwndLxEH_j7498ca7oU7vwuRJuIpUixqBP1fekFRlM21Oa4ea58wc4yQ8Fb-Q4h0vIxB68_XgvPTy2Tjc3crX-7h9lAvE5PSPoOl5biRmKfTjD6ucrM9RU8TxpXjOwxYQgzp0ExhTRo9Qmbr3lN-ltCWaHDcIBKJDujtkPd_sd6v4NOklnLGWqXw1AmiO1kMzglGodikKyH7acdjfzT6vjmr-Z__wf6pv40QgYAAA==.eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLUJSQVpJTC1TT1VUSC1CLVBSSU1BUlktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQiLCJleHAiOjE3MzQ0NzEyNzksImFsbG93QWNjZXNzT3ZlclB1YmxpY0ludGVybmV0Ijp0cnVlfQ==',
     tokenType: models.TokenType.Embed, // Tipo do token (Embed)
     settings: {
       panes: {
@@ -51,11 +51,11 @@ export class PowerbiReportComponent implements AfterViewInit {
       $schema: 'http://powerbi.com/product/schema#basic',
       filterType: models.FilterType.Basic,
       target: {
-        table: 'Funcionarios', // Substitua pelo nome da tabela
-        column: 'Nivel4', // Substitua pelo nome da coluna
+        table: 'Funcionarios',
+        column: 'Nivel4',
       },
       operator: 'In',
-      values: ['00020C08'], // Valores do filtro
+      values: ['00020C08'],
     };
 
     try {
